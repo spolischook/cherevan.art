88543d0f385197ecee6862dd5f6a3b88d5f037ea
Serhii Polishchuk <spolischook@gmail.com>
2024-02-27 09:30:05 +0100

    Removed time consuming debug

--- build.sh
+++ build.sh
@@ -8,14 +8,8 @@ npm install
 # Run the build command
 npm run build
 
-# List the files in the current directory
-ls -l
-
 # Run the image processing script
 node ./images_process.js
 
 # Run Hugo with the specified config file
 hugo -v --config=./hugo.yaml
-
-# List the files in the public directory
-tree public
